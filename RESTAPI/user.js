b50020e90d0f835f8352f82f6c5a4e000d4b85ad
michael.funk <michael.funk@cs.ucalgary.ca>
2021-12-16 00:43:12 -0700

    Basic Rest API setup / packages installed
    Installed: Node js, express, mysql, body-parser
    
    Sql database export saved for local hosting on workbench
    
    Currently localhost:3000/user returns example user in database

---
+++ RESTAPI/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const connection = require('../connection')
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+
+    connection.query('SELECT * FROM studyhubdb.user', (err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+
+
+});
+
+
+module.exports = router;
